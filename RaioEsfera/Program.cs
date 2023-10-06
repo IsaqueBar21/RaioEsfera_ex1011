f6fce52818a0c4a456f1023cd648d904c0761f24
Isaque Barbosa da Silva <isaque_barbosa@jabil.com>
2023-10-06 10:07:53 -0300

    primeiro commit

---
+++ RaioEsfera/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+double pi = 3.14159;
+double R, volume;
+
+Console.WriteLine("Insira abaixo o valor do Raio: ");
+R = double.Parse(Console.ReadLine());
+
+volume = (4 / 3.0) * pi * Math.Pow(R, 3);
+
+Console.WriteLine("VOLUME = " + volume.ToString("F3", CultureInfo.InvariantCulture));
+
